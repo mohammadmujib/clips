b18eca05337daf9296e9dce0823aa967831662c0
mohammadmujib <mohammadmujib09@gmail.com>
2022-10-06 16:05:48 +0530

    serviceVariable

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -8,12 +8,7 @@ import { ModalService } from '../services/modal.service'
   styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-  isAuthenticated = false
-  constructor(public modal: ModalService, public auth: AuthService) {
-    this.auth.isAuthenticated$.subscribe((status) => {
-      this.isAuthenticated = status
-    })
-  }
+  constructor(public modal: ModalService, public auth: AuthService) {}
 
   ngOnInit(): void {}
   openModal($event: Event) {
